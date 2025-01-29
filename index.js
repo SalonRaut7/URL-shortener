86cf402346cec56588f10886ee5e1042020c612b
Salon <rautsalon@gmail.com>
2025-01-29 18:33:11 +0545

    Added Server Side Rendering

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
 const { connectToMongoDB } = require('./connect');
 const urlRoute = require('./routes/url');
+const staticRoute = require('./routes/staticRouters');
 const URL = require('./models/url');
+
 
 const app = express();
 const PORT = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views',path.resolve('./views'));
+
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 connectToMongoDB('mongodb://127.0.0.1:27017/short-url')
 .then(() => console.log('MongoDb connected'));
 
 app.use('/url',urlRoute);
+app.use('/',staticRoute);
 
-app.get('/:shortId',async (req,res)=>{
+app.get('/url/:shortId',async (req,res)=>{
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate(
         {
